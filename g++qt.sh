5792dc8c7ecf635c2b6262911a0891e65716778b
you <you@example.com>
2017-08-02 12:42:40 +0900

    4th commit

--- g++qt.sh
+++ g++qt.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-export PATH="/opt/Qt/5.5/gcc/bin:"$PATH
-qmake -project
+sudo apt update 
+sudo apt install g++
+sudo apt install make
+sudo apt install libg1-mesa-dev
 
+export PATH="/opt/Qt/5.5/gcc/bin:"$PATH
+
+qmake -project
 #Qt5 & C++14に対応
 sed -i -e '$a QT += widgets' *.pro
 sed -i -e '$a CONFIG += c++14' *.pro
-
 qmake 
-
 make
 
 
